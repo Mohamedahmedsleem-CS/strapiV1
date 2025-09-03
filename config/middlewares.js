9c48906936d9b7106f457069294d19ddbc105588
Mohamedahmedsleem-CS <mohamedahmedsleem29511@gmail.com>
2025-09-04 00:21:08 +0300

    update middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,46 +3,71 @@ module.exports = [
   'strapi::errors',
   {
     name: 'strapi::security',
-     config: {
-       contentSecurityPolicy: {
-         useDefaults: true,
-         directives: {
-            'connect-src': ["'self'", 'https:'],
-            'img-src': [
-             "'self'",
-             'data:',
-             'blob:',
-             'https://market-assets.strapi.io',
-             'https://console.cloudinary.com',
-              'https://res.cloudinary.com',
-           ],
-           'script-src': [
-             "'self'",
-              'example.com',
-             'https://media-library.cloudinary.com',
-             'https://upload-widget.cloudinary.com',
-              'https://console.cloudinary.com',
-            ],
-          'media-src': ["'self'", 'data:', 'blob:', 'https://console.cloudinary.com'],
-           'frame-src': [
-             "'self'",
-            'https://media-library.cloudinary.com',
-             'https://upload-widget.cloudinary.com',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
             'https://console.cloudinary.com',
-           ],
-            upgradeInsecureRequests: null,
-          },
-       },
+            'https://res.cloudinary.com',
+            'https://strapi-95jv.onrender.com' // إضافة رابط Strapi المنشور
+          ],
+          'script-src': [
+            "'self'",
+            'example.com',
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'", 
+            'data:', 
+            'blob:', 
+            'https://console.cloudinary.com',
+            'https://strapi-95jv.onrender.com' // إضافة رابط Strapi المنشور
+          ],
+          'frame-src': [
+            "'self'",
+            'https://media-library.cloudinary.com',
+            'https://upload-widget.cloudinary.com',
+            'https://console.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
       },
-   },
-
-   {
+    },
+  },
+  {
     name: 'strapi::cors',
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:8080', 'https://digital-ecommerce-bice.vercel.app', '*']
-    }
+      // تحديث قائمة الأصول المسموح بها - إزالة النجمة '*' لتعزيز الأمان
+      origin: [
+        'http://localhost:8080',
+        'http://localhost:3000',
+        'http://localhost:1337',
+        'https://digital-ecommerce-bice.vercel.app',
+        'https://strapi-95jv.onrender.com'
+      ],
+      // إضافة إعدادات CORS المتقدمة
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      keepHeaderOnError: true,
+      credentials: true, // مهم للمصادقة
+      maxAge: 86400, // 24 ساعة
+      expose: [
+        'Content-Type',
+        'Authorization',
+        'X-Frame-Options',
+        'Origin',
+        'Accept'
+      ],
+    },
   },
   'strapi::poweredBy',
   'strapi::query',
